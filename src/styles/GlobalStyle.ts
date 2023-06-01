402b37419be8fd3d61c136f76ab64e9e4e353784
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-06-01 15:12:23 -0300

    initial setting

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+* { 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+}
+
+:focus {
+ outline: transparent;
+ box-shadow: 0 0 0 2px ${({ theme }) => theme.GREEN_SCALE[500]};
+}
+
+body, input, texarea, button {
+  font-size: 1rem;
+  font-weight: 400;
+  font-family: 'Roboto', sans-serif;
+
+  color: ${({ theme }) => theme.GREY_SCALE[300]};
+  background-color: ${({ theme }) => theme.GREY_SCALE[900]};
+}
+
+::-webkit-scrollbar {
+  width: 12px
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.GREY_SCALE[900]};
+  border: 1px solid ${({ theme }) => theme.GREY_SCALE[900]};
+}
+
+::-webkit-scrollbar-button {
+  background-color: ${({ theme }) => theme.GREY_SCALE[900]};
+}
+`
