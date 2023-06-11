5369227aed4c0ff0d89f40451f16589d467932b9
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-06-10 21:24:10 -0300

    Ajustes no layout

--- src/components/DefaultLayout/index.tsx
+++ src/components/DefaultLayout/index.tsx
@@ -1,12 +1,17 @@
 // Packages
 import { Outlet } from 'react-router-dom'
 
+// Components
+import { Header } from '../Header'
+
 // Styles
 import { ContainerSC } from './layoutStyles'
 
 export const DefaultLayout = () => {
   return (
     <ContainerSC>
+      <Header />
+
       <Outlet />
     </ContainerSC>
   )
