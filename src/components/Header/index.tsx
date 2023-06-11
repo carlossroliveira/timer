80a31b404aada3bc114a1e9aa4591af035123f1d
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2023-06-10 21:24:26 -0300

    Criação do componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+// Packages
+import { NavLink } from 'react-router-dom'
+import { Scroll, Timer } from 'phosphor-react'
+
+// Logo
+import logoIgnite from '../../assets/logo-ignite.svg'
+
+// Styles
+import { HeaderContainer } from './HeaderStyles'
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <span>
+        <img src={logoIgnite} alt="" />
+      </span>
+
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
